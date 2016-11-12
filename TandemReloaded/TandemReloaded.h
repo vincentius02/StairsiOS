cc020fbd06b3f8373adb266c3e859d8c3b237029
Yannick <yngeyer@gmail.com>
2016-11-12 14:25:30 +0900

    2

---
+++ TandemReloaded/TandemReloaded.h
@@ -0,0 +1,19 @@
+//
+//  TandemReloaded.h
+//  TandemReloaded
+//
+//  Created by Yannick Noah G. on 12.11.16.
+//  Copyright © 2016 Yannick Noah G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TandemReloaded.
+FOUNDATION_EXPORT double TandemReloadedVersionNumber;
+
+//! Project version string for TandemReloaded.
+FOUNDATION_EXPORT const unsigned char TandemReloadedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TandemReloaded/PublicHeader.h>
+
+
